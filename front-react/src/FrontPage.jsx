502afe9038f9a0a2dc4f40ef85aacfe7f347ec41
vctrubio <vctrubio@gmail.com>
2024-06-21 12:14:34 +0200

    render init of windLogo

--- front-react/src/FrontPage.jsx
+++ front-react/src/FrontPage.jsx
@@ -1,17 +1,46 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { showRow } from './Utils.jsx';
 import { paramsWindSpeed, paramsWindDirection, paramsCloudCover } from './ApiCall.js';
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 const Render = () => {
-    return (
-        <>hello div</>
-    );
-}
+    const mountRef = useRef(null);
+
+    useEffect(() => {
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        const renderer = new THREE.WebGLRenderer({alpha: true});
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        mountRef.current.appendChild(renderer.domElement);
+        const light = new THREE.AmbientLight(0xffffff); // Soft white light
+        scene.add(light);
+
+        const loader = new GLTFLoader();
+        loader.load('windLogo', (gltf) => {
+            scene.add(gltf.scene);
+            camera.position.set(0, 0, 1); // Adjust camera position as needed
+
+            const animate = function () {
+                requestAnimationFrame(animate);
+                renderer.render(scene, camera);
+            };
+            animate();
+        });
+
+        return () => {
+            mountRef.current.removeChild(renderer.domElement);
+        };
+    }, []);
+
+    return <div ref={mountRef}></div>;
+};
+
 
 function showWindSpeed(ptrWindHr) {
     let sum = 0;
     for (let param of paramsWindSpeed) {
-        console.log(ptrWindHr[param])
+        // console.log(ptrWindHr[param])
         sum += ptrWindHr[param];
     }
     const average = sum / paramsWindSpeed.length;
